f6e23fe7bcd30db85c7608142829d6783591b628
chenq <chenq@ersoft.cn>
2018-06-12 10:12:10 +0800

    添加一致性hash算法。

---
+++ src/main/java/com/github/yulechen/consistenthash/SortedMapTest.java
@@ -0,0 +1,24 @@
+package com.github.yulechen.consistenthash;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapTest {
+    private static SortedMap<Integer, String> virtualNodes = new TreeMap<Integer, String>();
+    public static void main(String[] args) {
+        virtualNodes.put(1,"a");
+        virtualNodes.put(2,"b");
+        virtualNodes.put(3,"c");
+        for(int i=0;i<10 ;i++){
+            SortedMap<Integer, String> integerStringSortedMap = null;
+            // 超过最大节点的值，使用第一个节点
+            // 构建hash 环
+            integerStringSortedMap = virtualNodes.tailMap(i>virtualNodes.lastKey()?
+                    virtualNodes.firstKey():i);
+            Integer integer = integerStringSortedMap.firstKey();
+            System.out.println(i+"----> "+integerStringSortedMap.get(integer));
+        }
+
+    }
+
+}
