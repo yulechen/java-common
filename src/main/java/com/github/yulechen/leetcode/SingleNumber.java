681471500671766be5266be1bb4afd583026c07d
chenq <chenq@ersoft.cn>
2018-11-29 16:00:49 +0800

    leetcode single number.

--- src/main/java/com/github/yulechen/leetcode/SingleNumber.java
+++ src/main/java/com/github/yulechen/leetcode/SingleNumber.java
@@ -47,10 +47,22 @@ public class SingleNumber {
     /**
      *
      * 异或+分组
+     * 是第一题的变种，一些出现两次，有两个数字a，b不同且各出现一次。先做一趟xor假设结果为x，
+     * 那么x是a，b亦或的结果，因为二者不同那么x中必然有一位是1，我们使用方法4得到这个1，
+     * 是一个只包含一位1的掩码。然后做第二趟xor，这次使用掩码分组操作。就可以区分a和b。
+     * 其余额元素也会分组，不过不影响结果，因为肯定是在某一组中出现两次
+     *
      * 相同为0，不同为1
      * 0 ^0 = 0
      * 1^0  = 1
      * 1^1  =  0
+     *
+     *
+     * 4.如何得到一个int的最右侧的第一个1？方法是与其相反数做&。因为相反数是取反加一，
+     * 取反以后，第一个1右侧的0变为1，加一以后这些1变为0，相应的第一个1变回为1，
+     * 比这个1高的位全是0，因为取反。最终就得到了一个只包含最右侧1的掩码，
+     * 其余是0。这个方法是O1的。
+     *
      */
     public int[] singleNumber2(int[] nums) {
         int[] res = new int[2];
@@ -58,11 +70,11 @@ public class SingleNumber {
         for(int i = 0 ; i < nums.length; i++){
             n ^= nums[i];
         }
-        // 找出最右边第一个1
+        // n 0001000: 只有一个1位，其余位全是0
         n = n & ~(n - 1);
         for(int i = 0 ; i < nums.length; i++){
             // 这一位是1的分一组
-            if((nums[i] & n) == n){
+            if((nums[i] & n) == n){ // & 掩码运算
                 res[0] ^= nums[i];
             } else {
                 // 不是1的分一组
@@ -74,11 +86,36 @@ public class SingleNumber {
     }
 
 
+    public int[] singleNumber3(int[] nums) {
+        int[] res = new int[2];
+        int n = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            n ^= nums[i];
+        }
+
+        int k=1;
+        while(n!=1){
+            k=k<<1;
+            n=n>>1;
+        }
+        for(int i = 0 ; i < nums.length; i++){
+            // 这一位是1的分一组
+            if((nums[i] & k) == k){ // & 掩码运算
+                res[0] ^= nums[i];
+            } else {
+                // 不是1的分一组
+                res[1] ^= nums[i];
+            }
+        }
+        return res;
+
+    }
+
 
     public static void main(String[] args) {
         SingleNumber sn =new SingleNumber();
         int[] a ={1,2,3,1,5,2};
-        int[] ints = sn.singleNumber2(a);
+        int[] ints = sn.singleNumber3(a);
         for (int anInt : ints) {
             System.out.println(anInt);
         }
