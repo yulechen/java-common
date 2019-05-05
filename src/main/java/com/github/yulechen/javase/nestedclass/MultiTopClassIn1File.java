f2e0b37cdad2e1aa64051a974d593d5d87d9d722
chenq <chenq@ersoft.cn>
2019-05-05 11:32:01 +0800

    multi class define in 1 file is a wrong practise

---
+++ src/main/java/com/github/yulechen/javase/nestedclass/MultiTopClassIn1File.java
@@ -0,0 +1,18 @@
+package com.github.yulechen.javase.nestedclass;
+
+/**
+ * java allow muilte class define in 1 file ,but only 1 class can be public
+ *
+ */
+
+/**
+ * never do this ,
+ */
+public class MultiTopClassIn1File {
+
+}
+
+// use nested class if possible or define in other file
+class B {
+
+}
